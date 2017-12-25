1ffb0a8564d35bb16bc6b28cc2f3f56d7f493d5a
Gregorio Bolivar <elalconxvii@gmail.com>
2017-12-25 12:36:19 -0300

    Subiendo mejoras del proceso integrando nueva funcionalidad para sqlite

--- lib/Core/Loading.php
+++ lib/Core/Loading.php
@@ -1,56 +1,48 @@
 <?php
 namespace Catatumbo\Core;
-/**
- * Clase encargada de gestionar todas la carga del sistema
- * @Author: Gregorio Bolívar <elalconxvii@gmail.com>
- * @Author: Blog: <http://gbbolivar.wordpress.com>
- * @Creation Date: 25/07/2017
- * @version: 0.7
- */
 
-class Loading implements Constant 
+class Loading implements Constant
 {
-	public function auth()
-	{
+    public function auth()
+    {
 
-	    $mw = function ($request, $response, $next) {
-		    $response->getBody()->write('BEFORE');
-		    $response = $next($request, $response);
-		    $response->getBody()->write('AFTER');
-		    return $response;
-		};
-        
-	}
-	
-	public  function init($app)
-	{
-		
-		$path = Constant::DIR_MODULE;
-		include_once (__DIR__."/main.php");
-		$config = parse_ini_file(Constant::DIR_CONFIG.'app.ini', true);
-		$dir = opendir($path);
-		$data[]=null;
+        $mw = function ($request, $response, $next) {
+            $response->getBody()->write('BEFORE');
+            $response = $next($request, $response);
+            $response->getBody()->write('AFTER');
+            return $response;
+        };
+
+    }
+    public  function init($app)
+    {
+
+        $path = Constant::DIR_MODULE;
+        include_once ($path."common".DIRECTORY_SEPARATOR."main.php");
+        $config = parse_ini_file(Constant::DIR_CONFIG.'app.ini', true);
+        $dir = opendir($path);
+        $data[]=null;
         // Leer todos los ficheros de la carpeta
-		while ($elemento = readdir($dir)){
+        while ($elemento = readdir($dir)){
             // Tratamos los elementos . y .. que tienen todas las carpetas
-			if( $elemento != "." && $elemento != ".." && $elemento != "common" && $elemento != "authentication"){
+            if( $elemento != "." && $elemento != ".." && $elemento != "common" && $elemento != "authentication"){
                 // Verificar si es una Carpeta
-				if( is_dir($path.$elemento) ){
+                if( is_dir($path.$elemento) ){
                     // Muestro la carpeta
-					$file = $path."".$elemento."/main.php";
-					$getFile=file_get_contents($file);
-					$getItem = $common->multiExplode(array("/**",'*/'),$getFile,$elemento,$config['default']['apiRestSi']);
-		            //$common->printAll($getItem);
+                    $file = $path."".$elemento.DIRECTORY_SEPARATOR."main.php";
+                    $getFile=file_get_contents($file);
+                    $getItem = $common->multiExplode(array("/**",'*/'),$getFile,$elemento,$config['default']['apiRestSi']);
+                    //$common->printAll($getItem);
 
-					if(!is_null($getItem)){
-						$data[$elemento]=$getItem;
-					}
-					include_once($file);
+                    if(!is_null($getItem)){
+                        $data[$elemento]=$getItem;
+                    }
+                    include_once($file);
 
-				} 
-			}
-		}
-		$_SESSION['data']=$data;
-	}
+                }
+            }
+        }
+        $_SESSION['data']=$data;
+    }
 
 }
--- modulos/common/main.php
+++ modulos/common/main.php
@@ -11,12 +11,12 @@ try {
     $common = new Common();
     $config = parse_ini_file(Constant::DIR_CONFIG.'app.ini', true);
 
-	$connec = new Database('catatumbo');
-	$module=$common->getFileExecute(__DIR__);
+    $connec = new Database('catatumbo');
+    $module=$common->getFileExecute(__DIR__);
 
-	$c = new \Slim\Container();
+    $c = new \Slim\Container();
 
-	$app->add(function (Request $request, Response $response, $next) use ($c) {
+    $app->add(function (Request $request, Response $response, $next) use ($c) {
         // First execute anything else
         $response = $next($request, $response);
         //print_r($response->getStatusCode()); die('kkj');
@@ -41,57 +41,57 @@ try {
 
 
     $app->get('/documents', function(Request $request, Response $response, array $args) use ($app,$config,$common){
-    	$data=$_SESSION['data'];
+        $data=$_SESSION['data'];
         return $this->renderer->render($response,'viewDocuments.php', array('apiDoc' => $data,'system'=>$config['default'],'common'=>$common));
     });
 
-/*$app->get('/login', function(Request $request, Response $response, array $args) use ($app,$config,$common,$connec){
-	$mensaje = '';
-	return $this->renderer->render($response,'viewLogin.php', array('system'=>$config['default'],'common'=>$common,'mensaje'=>$mensaje));
-});
+    /*$app->get('/login', function(Request $request, Response $response, array $args) use ($app,$config,$common,$connec){
+        $mensaje = '';
+        return $this->renderer->render($response,'viewLogin.php', array('system'=>$config['default'],'common'=>$common,'mensaje'=>$mensaje));
+    });
 
-$app->post('/proceforms', function(Request $request, Response $response, array $args) use ($app,$config,$common,$connec){
-	$conn = end($connec);
-	if($request->isPost())
-	{
-		$post = (object)$request->getParams();
-		if(isset($post->user) && isset($post->passwort))
-		{
-			$app->redirect('login');
-		}else{
-			// Capturando los datos para el registro de los usuarios
-			$user=$request->getParam('user');
-			$password=$request->getParam('password');
-
-
-        // Query para extraer todas las metas de la pagina web  
-			$sql = "SELECT usuario, clave FROM usuarios WHERE usuario='".$user."' AND clave='".$password."' ";
-			$query = $conn->prepare($sql);
-			$result=$query->execute();
-			/** Verifico si hay mas de un registro 
-			$rows = count($query->fetchAll(PDO::FETCH_ASSOC));
-			$connec->close_con();
-			if($rows==1){
-				return $response->withStatus(302)->withHeader('Location', '/admin');
-			}else{
-				$mensaje='Usuario y clave incorrecto intente nuevemente.';
-				return $this->renderer->render($response,'viewLogin.php', array('system'=>$config['default'],'common'=>$common,'mensaje'=>$mensaje));
-
-			}
-			
-		}
-	}
-});
+    $app->post('/proceforms', function(Request $request, Response $response, array $args) use ($app,$config,$common,$connec){
+        $conn = end($connec);
+        if($request->isPost())
+        {
+            $post = (object)$request->getParams();
+            if(isset($post->user) && isset($post->passwort))
+            {
+                $app->redirect('login');
+            }else{
+                // Capturando los datos para el registro de los usuarios
+                $user=$request->getParam('user');
+                $password=$request->getParam('password');
 
 
-$app->get('/admin', function (Request $request, Response $response, array $args) use ($app,$config,$common,$connec) {
-	return $this->renderer->render($response, 'viewBase.php', array('system'=>$config['default'],'common'=>$common));
-});
+            // Query para extraer todas las metas de la pagina web
+                $sql = "SELECT usuario, clave FROM usuarios WHERE usuario='".$user."' AND clave='".$password."' ";
+                $query = $conn->prepare($sql);
+                $result=$query->execute();
+                /** Verifico si hay mas de un registro
+                $rows = count($query->fetchAll(PDO::FETCH_ASSOC));
+                $connec->close_con();
+                if($rows==1){
+                    return $response->withStatus(302)->withHeader('Location', '/admin');
+                }else{
+                    $mensaje='Usuario y clave incorrecto intente nuevemente.';
+                    return $this->renderer->render($response,'viewLogin.php', array('system'=>$config['default'],'common'=>$common,'mensaje'=>$mensaje));
 
-$app->get('/logout', function () {
-    //Remove auth cookie and redirect to login page
-});*/
+                }
+
+            }
+        }
+    });
+
+
+    $app->get('/admin', function (Request $request, Response $response, array $args) use ($app,$config,$common,$connec) {
+        return $this->renderer->render($response, 'viewBase.php', array('system'=>$config['default'],'common'=>$common));
+    });
+
+    $app->get('/logout', function () {
+        //Remove auth cookie and redirect to login page
+    });*/
 
 } catch(\PDOException $e) {
-	die(print_r(json_encode(array('code' => 3, 'message'=> "Failed to connect to database ".$e->getMessage()))));
+    die(print_r(json_encode(array('code' => 3, 'message'=> "Failed to connect to database ".$e->getMessage()))));
 }
